485186679a3e4104538c42d3bab52807362137a5
Khrongpop <mr.khrongpop@gmail.com>
2019-11-14 11:39:29 +0700

    Initial Commit

---
+++ Hello=CoreMl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello=CoreMl
+//
+//  Created by Muang on 14/11/2562 BE.
+//  Copyright © 2562 ict. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
